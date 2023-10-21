28592014f4db79095505344f23eeb5bcaa5a887f
oboril <jan.oboril@gmail.com>
2023-10-21 13:35:01 +0200

    initial commit

---
+++ system.cs
@@ -0,0 +1,197 @@
+namespace SystemSimulation
+{
+    using Probability = Double;
+    using Rate = Double;
+    using Time = Double;
+    using ElementTypeID = Int32;
+    using ElementID = Int32;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Defines a single possible states with specific failure causes
+    /// </summary>
+    struct State
+    {
+        /// <summary>
+        /// Bitmap, if (individual_failures >> idx & 1) the element is in failed state.
+        /// </summary>
+        public readonly ulong individual_failures;
+        /// <summary>
+        /// Bitmap, if (common_failures >> idx & 1) the element type is in failed state.
+        /// </summary>
+        public readonly ulong common_failures;
+
+        /// <summary>
+        /// Creates bitmap where (result >> idx) & 1 denotes whether given element is in failed state
+        /// </summary>
+        public static ulong all_element_states(ulong common_failures, ulong individual_failures, List<ElementTypeID> elements)
+        {
+            ulong all_element_states = 0;
+            for (int element = 0; element < elements.Count(); element++)
+            {
+                ulong failed = ((individual_failures >> element) & 1) | ((common_failures >> elements[element]) & 1);
+                all_element_states |= failed >> element;
+            }
+            return all_element_states;
+        }
+
+        public State(ulong common_failures_, ulong individual_failures_)
+        {
+            common_failures = common_failures_;
+            individual_failures = individual_failures_;
+        }
+
+        /// <summary>
+        /// Calculates probability of this state
+        /// </summary>
+        public Probability probability(List<ElementType> element_types, List<ElementTypeID> elements, Time mission_time, Time time_since_proof_test)
+        {
+            Probability prob = 1.0;
+
+            // Individual failures
+            for (int element = 0; element < elements.Count(); element++)
+            {
+                Probability p = element_types[elements[element]].independent_failure(mission_time, time_since_proof_test);
+                if (((individual_failures >> element) & 1) == 1)
+                    prob *= p;
+                else
+                    prob *= 1.0 - p;    
+            }
+
+
+            // Common failuers
+            for (int element_type = 0; element_type < element_types.Count(); element_type++)
+            {
+                Probability p = element_types[element_type].common_failure(mission_time, time_since_proof_test);
+                if (((common_failures >> element_type) & 1) == 1)
+                    prob *= p;
+                else
+                    prob *= 1.0 - p;    
+            }
+
+
+            return prob;
+        }
+    }
+
+    class SILSystem
+    {
+        /// <summary>
+        /// List of all element types. This stores failure probalities.
+        /// </summary>
+        List<ElementType> element_types;
+        /// <summary>
+        /// Types of all elements in the system.
+        /// </summary>
+        List<ElementTypeID> elements;
+        /// <summary>
+        /// Final voting that determines whether the system failed.
+        /// </summary>
+        Voting? final_voting;
+
+        bool compiled;
+        /// <summary>
+        /// List of all significatly contributing states that cause failure
+        /// </summary>
+        List<State> fail_states;
+
+        /// <summary>
+        /// Creates empty SIL system class.
+        /// </summary>
+        SILSystem()
+        {
+            element_types = new List<ElementType>();
+            elements = new List<ElementTypeID>();
+            final_voting = null;
+            compiled = false;
+            fail_states = new List<State>();
+        }
+
+        /// <summary>
+        /// Adds new type of element to the SIL system
+        /// </summary>
+        /// <param name="element_type">The specifications of the element type</param>
+        /// <returns>ID of the new element type</returns>
+        public ElementTypeID add_element_type(ElementType element_type)
+        {
+            if (compiled)
+                throw new Exception("Cannon add elements to compiled system");
+            element_types.Add(element_type);
+            return element_types.Count() - 1;
+        }
+
+        /// <summary>
+        /// Adds element of given type to the SIL system.
+        /// </summary>
+        /// <param name="element_type_id">ID of the type of the element, as returned by add_element_type()</param>
+        /// <returns>ID of the added element</returns>
+        public ElementID add_element(ElementTypeID element_type_id)
+        {
+            if (compiled)
+                throw new Exception("Cannon add elements to compiled system");
+            elements.Add(element_type_id);
+            return elements.Count() - 1;
+        }
+
+        /// <summary>
+        /// Sets the final voting that determines whether the SIL system failed or not.
+        /// </summary>
+        /// <param name="voting">The final voting.</param>
+        public void set_final_voting(Voting voting)
+        {
+            if (compiled)
+                throw new Exception("Cannon change final voting of compiled system");
+            final_voting = voting;
+        }
+
+        /// <summary>
+        /// Compiles the SIL system, this is needs to be done before calculating probabilities.
+        /// Compiling the SIL system significantly speeds up calculation of failure probabilities, which is key for fast integration.
+        /// </summary>
+        public void compile()
+        {
+            // Check that final voting is set
+            if (! final_voting.HasValue)
+                throw new Exception("The final voting needs to be specified before SIL system is compiled");
+
+            // Check number of states
+            int vars = element_types.Count() + elements.Count();
+            if (vars > 40)
+                throw new Exception("Using more than 40 elements and element types in SIL system will take forever.");
+            if (vars > 20)
+                Console.WriteLine("WARNING: the number of elements in SIL system is high {}, this will cause compilation to be very slow", vars);
+            
+            // Cache all failing states
+            for (ulong state_idx = 0; state_idx < (1ul << vars); state_idx++)
+            {
+                ulong common = state_idx & ((1ul << element_types.Count()) - 1);
+                ulong individual = state_idx >> element_types.Count();
+
+                ulong all_element_states = State.all_element_states(common, individual, elements);
+                if (final_voting.Value.failed(all_element_states))
+                {
+                    State state = new State(common, individual);
+                    fail_states.Add(state);
+                }
+            }
+
+            // Set flags
+            compiled = true;
+        }
+
+        public Probability failure_probability(Time mission_time, Time time_since_proof_test)
+        {
+            // Check that system is compiled 
+            if (! compiled)
+                throw new Exception("The system needs to be compiled before calculating probabilities");
+            
+            // Sum probabilities of all fail states
+            Probability prob = 0.0;
+
+            foreach (State state in fail_states)
+                prob += state.probability(element_types, elements, mission_time, time_since_proof_test);
+
+            return prob;
+        }
+    }
+}
---
+++ types.cs
@@ -0,0 +1,143 @@
+namespace SystemSimulation
+{
+    using Probability = Double;
+    using Rate = Double;
+    using Time = Double;
+    using ElementTypeID = Int32;
+    using ElementID = Int32;
+
+    struct ElementType
+    {
+        /// <summary>
+        /// Rate of dangerous detected failures, both due to individual and common causes.
+        /// Corresponding symbol is lambda_DD.
+        /// </summary>
+        Rate dangerous_detected;
+        /// <summary>
+        /// Rate of dangerous undetected failures, both due to individual and common causes.
+        /// Note that some of these failures will be detected during proof tests.
+        /// Corresponding symbol is lambda_DU
+        /// </summary>
+        Rate dangerous_undetected;
+        /// <summary>
+        /// Defines what fraction of dangerous undetected failures will be detected during proof tests.
+        /// Corresponding symbol is PTC.
+        /// </summary>
+        Probability proof_test_coverage;
+        /// <summary>
+        /// Defines what fraction of undetected failures is due to common causes.
+        /// Corresponding symbol is beta.
+        /// </summary>
+        Probability common_undetected;
+        /// <summary>
+        /// Defines what fraction of detected failures is due to common causes.
+        /// Corresponding symbol is beta_D.
+        /// </summary>
+        Probability common_detected;
+        /// <summary>
+        /// Time required to repair detected failure.
+        /// Corresponding symbol is MRT.
+        /// </summary>
+        Time mean_repair_time;
+        /// <summary>
+        /// Time required to repair undetected failure, presumably detected during proof tests.
+        /// However, an approximation is made that assumes that the failures are repaired immidiately.
+        /// Corresponding symbol is MTTR.
+        /// </summary>
+        Time mean_time_to_restore;
+
+        public ElementType(Rate dangerous_detected_, Rate dangerous_undetected_, Probability proof_test_coverage_, Probability common_detected_, Probability common_undetected_, Time mean_repair_time_, Time mean_time_to_restore_)
+        {
+            dangerous_detected = dangerous_detected_;
+            dangerous_undetected = dangerous_undetected_;
+            proof_test_coverage = proof_test_coverage_;
+            common_detected = common_detected_;
+            common_undetected = common_undetected_;
+            mean_repair_time = mean_repair_time_;
+            mean_time_to_restore = mean_time_to_restore_;
+        }
+
+        Probability exponential_dist(Rate r, Time t)
+        {
+            Probability rt = r * t;
+            // Using Taylor expansion of 1 - exp(-rt) is most numerically stable
+            return rt - rt * rt / 2 + rt * rt * rt / 6 - rt * rt * rt * rt / 24;
+        }
+
+        /// <summary>
+        /// Calculates probability that a single element of this type is in the failed state at given time.
+        /// </summary>
+        public Probability independent_failure(Time mission_time, Time time_since_proof_test)
+        {
+            Probability combined = 0.0d;
+
+            // Undetected failures
+            combined += exponential_dist(dangerous_undetected * proof_test_coverage * (1 - common_undetected), time_since_proof_test);
+            combined += exponential_dist(dangerous_undetected * (1 - proof_test_coverage) * (1 - common_undetected), mission_time);
+
+            // Repair times
+            combined += mean_repair_time * dangerous_detected * (1 - common_detected);
+            combined += mean_time_to_restore * dangerous_undetected * (1 - common_undetected);
+
+            return combined;
+        }
+
+        /// <summary>
+        /// Calculates probability that all elements of this type is in the failed state at given time due to a common cause.
+        /// </summary>
+        public Probability common_failure(Time mission_time, Time time_since_proof_test)
+        {
+            Probability combined = 0.0d;
+
+            // Undetected failures
+            combined += exponential_dist(dangerous_undetected * proof_test_coverage * common_undetected, time_since_proof_test);
+            combined += exponential_dist(dangerous_undetected * (1 - proof_test_coverage) * common_undetected, mission_time);
+
+            // Repair times
+            combined += mean_repair_time * dangerous_detected * common_detected;
+            combined += mean_time_to_restore * dangerous_undetected * common_undetected;
+
+            return combined;
+        }
+    }
+
+    struct Voting
+    {
+        /// <summary>
+        /// Redundancy - how many inputs can fail without this voting failing.
+        /// </summary>
+        int redundancy;
+        /// <summary>
+        /// List of Element IDs that this voting takes as an input
+        /// </summary>
+        List<ElementID> elements;
+        /// <summary>
+        /// List of IDs of votings that this voting takes as an input
+        /// </summary>
+        List<Voting> votings;
+
+        /// <summary>
+        /// Determines whether the voting failed based on current inputs
+        /// </summary>
+        /// <param name="all_element_states">Bitmap, states whether each of the elements failed</param>
+        /// <param name="all_votings">List of all votings in the system</param>
+        /// <returns>Returns true if voting failed</returns>
+        public bool failed(ulong all_element_states)
+        {
+            int failed_count = 0;
+
+            foreach (ElementID id in elements)
+                 if (((all_element_states >> id) & 1) == 1)
+                    failed_count++;
+
+            foreach (Voting voting in votings)
+                 if (voting.failed(all_element_states))
+                    failed_count++;
+
+            if (failed_count > redundancy)
+                return true;
+            else
+                return false;
+        }
+    }
+}
